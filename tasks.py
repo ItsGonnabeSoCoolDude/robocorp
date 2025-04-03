15fa5d1509670420703aba894f33abaddbec99f9
ItsGonnabeSoCoolDude <56778781+ItsGonnabeSoCoolDude@users.noreply.github.com>
2025-04-03 11:21:52 -0500

    Add RPA Workflow (#3)
    * Add RPA Workflow
    
    * formatted tasks.py with black
    
    * auto-format with black

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from robocorp.tasks import task
 from robocorp import browser
 
+
 @task
 def robot_spare_bin_python():
     """Insert the sales data for the week and export it as a PDF"""
@@ -11,16 +12,19 @@ def robot_spare_bin_python():
     log_in()
     fill_and_submit_sales_form()
 
+
 def open_the_intranet_website():
     """Navigates to the given URL"""
     browser.goto("https://robotsparebinindustries.com/")
+
 
 def log_in():
     """Fills in the login form and clicks the 'Log in' button"""
     page = browser.page()
     page.fill("#username", "maria")
     page.fill("#password", "thoushallnotpass")
-    page.click("button:text('Log in')") 
+    page.click("button:text('Log in')")
+
 
 def fill_and_submit_sales_form():
     """Fills in the sales data and click the 'Submit' button"""
